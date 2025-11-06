afdb9363c3febaa37c3562fb95c7ee66c09b1025
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-11-06 19:26:25 +0100

    bump version 2.1.1 -> 2.1.2

--- stahl_ankifier.py
+++ stahl_ankifier.py
@@ -44,7 +44,7 @@ from PIL import Image
 from tqdm import tqdm
 
 # Version of the script
-VERSION = "2.1.1"
+VERSION = "2.1.2"
 
 
 def _clean_page_headers(soup: BeautifulSoup, drug_name: str) -> BeautifulSoup:
